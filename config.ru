e2b2b8a423058e1a3f9c3ea965d7b4633dc49a3b
Derek Eder <derek.eder@gmail.com>
2012-12-20 02:14:20 -0600

    ported clearstreets to Fusion Table Heroku template, added plow trace page

--- config.ru
+++ config.ru
@@ -7,4 +7,12 @@ $:.unshift File.join(base, "lib")
 require "fusion_template"
 
 Sinatra::Base.set(:root) { base }
+
+if memcache_servers = ENV["MEMCACHE_SERVERS"]
+  use Rack::Cache,
+    verbose: true,
+    metastore:   "memcached://#{memcache_servers}",
+    entitystore: "memcached://#{memcache_servers}"
+end
+
 run FusionTemplate::Application
--- lib/fusion_template/application.rb
+++ lib/fusion_template/application.rb
@@ -25,20 +25,22 @@ module FusionTemplate
     helpers FusionTemplate::HtmlHelpers
     
     get "/" do
+      cache_control :public, max_age: 3600  # 1 hour
       @current_menu = "home"
       haml :index
     end
 
     # note: the fusion_tables gem can only access tables based on the numeric ID of the table
-    get "/location_list" do
-      @current_menu = "location_list"
-      if defined? FT
-        # note: the fusion_tables gem can only access tables based on the numeric ID of the table
-        @recycling_locations = FT.execute("SELECT * FROM 2086698;")
-        haml :location_list
-      else
-        "fusion_tables gem not setup yet! You need to set your Google account and password in config/config.yml"
-      end
+    get "/plows" do
+      cache_control :public, max_age: 3600  # 1 hour
+      @plows = FT.execute("SELECT 'Plow ID', Count() FROM 129O2iei3jOpvTT_yG2xjHjY2hvunTb4IzTHBj_A GROUP BY 'Plow ID' ORDER BY Count() DESC")
+      haml :plows
+    end
+
+    get "/about" do
+      cache_control :public, max_age: 3600  # 1 hour
+      @current_menu = "about"
+      haml :about
     end
     
     get "/:page" do
