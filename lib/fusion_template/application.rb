38477e8c9fd6091207c17d0ab0d4551cd65a621a
Derek Eder <derek.eder@gmail.com>
2014-02-07 15:35:50 -0600

    cleanup for feb 5 storm

--- lib/fusion_template/application.rb
+++ lib/fusion_template/application.rb
@@ -25,6 +25,7 @@ module FusionTemplate
     helpers FusionTemplate::HtmlHelpers
 
     Past_storms = [
+      {:title => 'Feb 4-5, 2014', :backup_folder => '02-07-2014', :snowfall => '7.1"'},
       {:title => 'Jan 31 - Feb 2, 2014', :backup_folder => '02-04-2014', :snowfall => '4"'},
       {:title => 'Jan 24-26, 2014', :backup_folder => '01-27-2014', :snowfall => '2.8"'},
       {:title => 'Jan 21-23, 2014', :backup_folder => '01-24-2014', :snowfall => '3.1"'},
