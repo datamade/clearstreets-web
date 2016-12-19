b2c47678ae89bcc119aea8f4b913ac72da609f9b
Derek Eder <derek.eder@gmail.com>
2016-12-19 11:27:37 -0600

    timezones, yo

--- public/javascripts/cartodb_lib.js
+++ public/javascripts/cartodb_lib.js
@@ -39,7 +39,8 @@ var CartoDbLib = {
         var date_formatted = '';
         if (props) {
           // date_formatted = new moment(props.date_stamp).format("h:mm:ss a M/D/YYYY");
-          date_formatted = new moment(props.date_stamp, "YYY-MM-DD h:mm:ss+00").format("h:mm:ss a M/D/YYYY");
+          console.log(props)
+          date_formatted = new moment(props.date_stamp, "YYY-MM-DD h:mm:ss+00").subtract(5, 'hour').format("h:mm:ss a M/D/YYYY");
         }
         
         this._div.innerHTML = '<h4>Plow info</h4>' +  (props ?
